d22553ab9cda209bc75fb8712dfaca67eaf5e35a
R Somers <spamhole@telenet.be>
2012-04-28 12:24:03 +0200

    added usage to readme and remoived obsolete settings from settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,3 @@
-var settings_BASE_URL="http://localhost/web/CSRF_Scan2/"
 var settings_range="192.168.0.";
-var settings_reporturl="http://localhost/web/csrf_scan2/track.aspx";
+var settings_reporturl=""; 		//todo include this in logging routine to automaticaaly use when a value is specified
 var settings_death_timeout=4;
