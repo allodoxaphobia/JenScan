6ddd0647df2dc6e7f438edce1feb4187905b15aa
R Somers <spamhole@telenet.be>
2012-05-01 16:56:54 +0200

    include images.js in push might help :/

---
+++ images.js
@@ -0,0 +1,15 @@
+var images = new Array(
+"0|IIS7|80|http|welcome.png|0,0|0|",
+"1|IIS6|80|http|pagerror.gif|0,0|1|!0",
+"2|APACHE|80|http|icons/apache_pb.gif|0,0|1|!0",
+"3|APACHE 2.x|80|http|icons/apache_pb2.gif|0,0|1|!0",
+"4|UNIDENTIFIED|80|http|favicon.ico|0,0|1|0",
+"5|UNIDENTIFIED|80|http|favicon.ico|0,0|1|!0",
+"6|Belkin Router|80|http|images/title_2.gif|321,28|0|!4",
+"7|Billion Router|80|http|customized/logo.gif|318,69|0|!4",
+"8|IR C2880|80|http|en/media/img140.gif|158,160|0|",
+"9|Traffic Grapher|80|http|images/prtg.gif|0,0|0|5",
+"10|Netgear ReadyNAS|443|https|images/NG/clear.gif|0,0|0|!0"
+)
+
+main_filesloaded =main_filesloaded+1
